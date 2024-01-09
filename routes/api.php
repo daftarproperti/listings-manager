68b845aa6ef45960461630a3aee4fb55aa1db276
AjiPrakosa <aji@jelajahrumah.id>
2024-01-09 10:29:23 +0700

    Fix Wrong Middleware Implementation

--- routes/api.php
+++ routes/api.php
@@ -18,10 +18,10 @@ Route::prefix('webhook')->group(function () {
     Route::post('tasks/gpt', 'Api\WebhookController@processGpt')->middleware('queue-webhook');
 });
 
-Route::prefix('tele-app')->group(function () {
+Route::group(['prefix' => 'tele-app', 'middleware' => ['telegram-app']], function () {
     Route::prefix('properties')->group(function () {
         Route::get('/', 'Api\PropertiesController@index');
         Route::get('/{id}', 'Api\PropertiesController@show');
     });
     Route::get('photo/{fileId}/{fileUniqueId}', 'Api\PhotoController@telegramPhoto')->name('telegram-photo');
-})->middleware('telegram-app');
+});
