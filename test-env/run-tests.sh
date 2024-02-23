ed92036d67955eb71d9dcc1acad34fe75bbde845
Sonny Sasaka <sonny@jelajahrumah.id>
2024-02-22 19:55:14 -0800

    Update test image with node and npm
    Change-Id: I66d56fbb133915290e6044ae271a7c5be6c34b58

--- test-env/run-tests.sh
+++ test-env/run-tests.sh
@@ -3,6 +3,10 @@
 
 composer install
 
+# To make sure vite manifest is created
+npm install
+npm run build
+
 ./test-env/wait-mongodb.sh || { echo "MongoDB did not become available, exiting..."; exit 1; }
 
 # mongodb is ready, do the test
