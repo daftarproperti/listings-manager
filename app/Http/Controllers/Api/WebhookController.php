18b17d66c44b6cb761cf9833581ff911acdbf4bc
Sonny Sasaka <sonny@jelajahrumah.id>
2024-02-29 16:51:03 -0800

    chore: Simplify exception handling in WebhookController
    Since there is no special handling of exception, no need to catch it and
    just let Laravel handle it to return the correct error.
    
    Change-Id: Ic1826f59f43bad4e5f235721b02a9bb26125a902

--- app/Http/Controllers/Api/WebhookController.php
+++ app/Http/Controllers/Api/WebhookController.php
@@ -8,7 +8,6 @@ use App\Http\Controllers\Controller;
 use App\Http\Services\ReceiveMessageService;
 use App\Jobs\ParseListingJob;
 use App\Models\ListingUser;
-use App\Models\PropertyUser;
 use App\Models\RawMessage;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
@@ -20,77 +19,72 @@ class WebhookController extends Controller
         Request $request,
         ReceiveMessageService $receiveMessageService,
     ): JsonResponse {
-        try {
-            $params = $request->validate([
-                'update_id' => 'required',
-                'message' => 'nullable',
-                'callback_query' => 'nullable',
-            ]);
+        $params = $request->validate([
+            'update_id' => 'required',
+            'message' => 'nullable',
+            'callback_query' => 'nullable',
+        ]);
 
-            $update = Update::from($params);
+        $update = Update::from($params);
 
-            //to avoid same message processing
-            $dataExists = RawMessage::where('update_id', $update->update_id)->exists();
-            if ($dataExists) {
-                Log::warning("Update id {$update->update_id} already exists, ignoring.");
-                return response()->json(['success' => true], 200);
+        //to avoid same message processing
+        $dataExists = RawMessage::where('update_id', $update->update_id)->exists();
+        if ($dataExists) {
+            Log::warning("Update id {$update->update_id} already exists, ignoring.");
+            return response()->json(['success' => true], 200);
+        }
+
+        //when message contain photo use 'caption' as message, because 'text' is not available.
+        $message = !empty($update->message->caption) ? $update->message->caption : ($update->message->text ?? '');
+
+        //to do next: use AI to check message is about property informations or not.
+        $isPropertyInformationMessage = $receiveMessageService
+            ->isPropertyInformationMessage(
+                $message,
+                10
+            );
+
+        if ($isPropertyInformationMessage) {
+            $message = $receiveMessageService->saveRawMessage($update);
+
+            $pictureUrls = [];
+            if (!empty($update->message->photo)) {
+                $pictureUrls = $receiveMessageService->pictureUrls($update->message->photo);
             }
 
-            //when message contain photo use 'caption' as message, because 'text' is not available.
-            $message = !empty($update->message->caption) ? $update->message->caption : ($update->message->text ?? '');
+            $template = storage_path('HousePropertyGptTemplate.txt');
+            $templateString = file_get_contents($template);
 
-            //to do next: use AI to check message is about property informations or not.
-            $isPropertyInformationMessage = $receiveMessageService
-                ->isPropertyInformationMessage(
-                    $message,
-                    10
-                );
+            $mainPrompt = sprintf(
+                '%s%s',
+                $update->message->text ?? '',
+                !empty($pictureUrls) ? "\n Picture Urls:\n" . implode("\n", $pictureUrls) . "\n" : ''
+            );
 
-            if ($isPropertyInformationMessage) {
-                $message = $receiveMessageService->saveRawMessage($update);
+            $listingUser = new ListingUser();
+            $listingUser->name = trim(sprintf(
+                '%s %s',
+                $update->message->from->first_name,
+                $update->message->from->last_name ?? ''
+            ));
+            $listingUser->userName = $update->message->from->username ?? null;
+            $listingUser->userId = $update->message->from->id;
+            $listingUser->source = 'telegram';
 
-                $pictureUrls = [];
-                if (!empty($update->message->photo)) {
-                    $pictureUrls = $receiveMessageService->pictureUrls($update->message->photo);
-                }
+            $chatId = isset($update->message->chat) ? $update->message->chat->id : null;
 
-                $template = storage_path('HousePropertyGptTemplate.txt');
-                $templateString = file_get_contents($template);
+            ParseListingJob::dispatch(
+                'Please give me json only also trim the value' . "\n" .
+                    $mainPrompt . "\n\n" . 'with following format:' . "\n\n" . $templateString,
+                $listingUser,
+                $chatId
+            );
 
-                $mainPrompt = sprintf(
-                    '%s%s',
-                    $update->message->text ?? '',
-                    !empty($pictureUrls) ? "\n Picture Urls:\n" . implode("\n", $pictureUrls) . "\n" : ''
-                );
-
-                $listingUser = new ListingUser();
-                $listingUser->name = trim(sprintf(
-                    '%s %s',
-                    $update->message->from->first_name,
-                    $update->message->from->last_name ?? ''
-                ));
-                $listingUser->userName = $update->message->from->username ?? null;
-                $listingUser->userId = $update->message->from->id;
-                $listingUser->source = 'telegram';
-
-                $chatId = isset($update->message->chat) ? $update->message->chat->id : null;
-
-                ParseListingJob::dispatch(
-                    'Please give me json only also trim the value' . "\n" .
-                        $mainPrompt . "\n\n" . 'with following format:' . "\n\n" . $templateString,
-                    $listingUser,
-                    $chatId
-                );
-
-                if ($chatId) {
-                    TelegramInteractionHelper::sendMessage($chatId, 'Terimakasih atas informasi yang diberikan.' . "\n" . 'Informasi sedang kami proses.');
-                }
-            } else {
-                Log::info('is not property informations: ' . print_r($update, TRUE));
+            if ($chatId) {
+                TelegramInteractionHelper::sendMessage($chatId, 'Terimakasih atas informasi yang diberikan.' . "\n" . 'Informasi sedang kami proses.');
             }
-        } catch (\Throwable $e) {
-            Log::error($e->getMessage());
-            return response()->json(['error' => $e->getMessage()], $e->status ?? 500);
+        } else {
+            Log::info('is not property informations: ' . print_r($update, TRUE));
         }
 
         return response()->json(['success' => true], 200);
