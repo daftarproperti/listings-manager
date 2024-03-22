4061fa02f167db6a92e13e36e2b1a2a7f9a7c4bb
Sonny Sasaka <sonny@jelajahrumah.id>
2024-03-21 23:42:02 -0700

    Add coverage generation script
    Add a script to generate coverage report, upload to artifacts.jlrm.net,
    and link the badge to README.md.
    
    Change-Id: I149bc882fae24ff1f8484954bda08874b734b473

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
+[![Coverage](https://img.shields.io/badge/dynamic/json?url=https%3A%2F%2Fartifacts.jlrm.net%2Fpublic%2Fcoverage.json&query=coverage&label=Coverage)](https://artifacts.jlrm.net/coverage)
+
 ## Daftar Properti
 
 This is the backend system of Daftar Properti:
---
+++ scripts/generate-coverage.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+TEMP_DIR=$(mktemp -d)
+echo Using temp dir $TEMP_DIR
+
+echo Calculating total percentage
+percent=$(php artisan test --coverage | grep Total: | grep -oP '\d+\.\d+ %')
+echo Total = $percent
+echo "{\"coverage\": \"$percent\"}" > $TEMP_DIR/coverage.json
+
+echo Generating HTML report
+php artisan test --coverage-html $TEMP_DIR/
+
+echo Copying to artifacts.jlrm.net
+rsync -e 'ssh -p 2222' -rlpvz --delete $TEMP_DIR/ root@artifacts.jlrm.net:/usr/share/nginx/html/coverage/
+scp -P 2222 $TEMP_DIR/coverage.json root@artifacts.jlrm.net:/usr/share/nginx/html/public/coverage.json
