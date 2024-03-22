075a7870f2f4ae17ab6fc67986eddb1d635af52d
Sonny Sasaka <sonny@jelajahrumah.id>
2024-03-22 13:41:39 -0700

    Fix coverage generator script permission issue
    Change-Id: Ia99f7a49b46fac934e26281825740307d537eae2

--- scripts/generate-coverage.sh
+++ scripts/generate-coverage.sh
@@ -12,5 +12,6 @@ echo Generating HTML report
 php artisan test --coverage-html $TEMP_DIR/
 
 echo Copying to artifacts.jlrm.net
+chmod 755 $TEMP_DIR # mktemp creates with 700 by default, we need to relax it to be readable by web server
 rsync -e 'ssh -p 2222' -rlpvz --delete $TEMP_DIR/ root@artifacts.jlrm.net:/usr/share/nginx/html/coverage/
 scp -P 2222 $TEMP_DIR/coverage.json root@artifacts.jlrm.net:/usr/share/nginx/html/public/coverage.json
