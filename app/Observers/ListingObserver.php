405b267866124a614dc9347a9d5eb27ac88e1014
Aji Prakosa <trisnaajiprakosa@gmail.com>
2024-03-27 12:05:28 +0700

    Sync to property when listing updated
    Change-Id: I782c6fd47052c104217503d18642318bde5b641a

--- app/Observers/ListingObserver.php
+++ app/Observers/ListingObserver.php
@@ -16,28 +16,55 @@ class ListingObserver
     public function created(Listing $listing): void
     {
         try {
-            $dataToCopy = collect($listing->toArray())->except(['_id', 'user', 'created_at', 'updated_at']);
-            $convertObjectKey = ['contact', 'coordinate'];
 
             $property = new Property();
-
-            $dataToCopy = $dataToCopy->map(function ($value, $key) use ($convertObjectKey, &$property) {
-                if (in_array($key, $convertObjectKey)) {
-                    $property->{$key} = (object) $value;
-                    return;
-                }
-                $property->{$key} = $value;
-                return;
-            });
+            $this->fillPropertyFromListing($listing, $property);
 
             /**
              * in next iteration 1 property will have more than 1 listing, so we use array for listings attribute
              */
             $property->listings = [Assert::string($listing->id)];
-
             $property->save();
+
         } catch (\Throwable $th) {
             Log::error('Error copy to property: ' . $th->getMessage());
         }
+    }
+
+    /**
+     * Handle the Listing "updated" event.
+     */
+    public function updated(Listing $listing): void
+    {
+        try {
+
+            $property = Property::where('listings', $listing->id)->first();
+            $this->fillPropertyFromListing($listing, $property);
+            $property->save();
+
+        } catch (\Throwable $th) {
+            Log::error('Error sync to property: ' . $th->getMessage());
+        }
+    }
+
+    /**
+     * @param Listing $listing
+     * @param Property $property
+     * @return void
+     */
+    private function fillPropertyFromListing(Listing $listing, Property &$property): void
+    {
+        $dataToSync = collect($listing->toArray())->except(['_id', 'user', 'created_at', 'updated_at']);
+
+        $convertObjectKey = ['contact', 'coordinate'];
+
+        $dataToSync->map(function ($value, $key) use ($convertObjectKey, &$property) {
+            if (in_array($key, $convertObjectKey)) {
+                $property->{$key} = (object) $value;
+                return;
+            }
+            $property->{$key} = $value;
+            return;
+        });
     }
 }
