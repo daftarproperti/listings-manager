ebe279e44800430015cb352f7a99da4a268dd8c4
Yohana Vitrawati <vivi@jelajahrumah.id>
2024-04-18 15:25:14 +0700

    feat: template with sidebar
    Change-Id: Ic0a65b4a4a418d05e1bff779a68e24a98e8a4135

---
+++ resources/views/partials/sidebar.blade.php
@@ -0,0 +1,12 @@
+<div class="px-6 pt-10 pb-10">
+    <h2 class="font-medium text-lg mb-1">Sidebar Title</h2>
+    <ul class="list-none leading-relaxed">
+        <li class="py-2">
+            <a href="/" class="text-blue-500 hover:text-blue-500">Link 1</a>
+        </li>
+        <li class="py-2"><a href="/" class="text-slate-700 hover:text-blue-500 active:text-blue-500">Link 2</a></li>
+        <li class="py-2"><a href="/" class="text-slate-700 hover:text-blue-500 active:text-blue-500">Link 3</a></li>
+        <li class="py-2"><a href="/" class="text-slate-700 hover:text-blue-500 active:text-blue-500">Link with long text</a></li>
+        <li class="py-2"><a href="/" class="text-slate-700 hover:text-blue-500 active:text-blue-500">Link with super long long text until second row</a></li>
+    </ul>
+</div>
---
+++ resources/views/templates/withSidebar.blade.php
@@ -0,0 +1,89 @@
+<div class="flex-none md:flex">
+    <div class="w-full md:w-3/4 pr-0 md:pr-4">
+        <article class="mt-2 text-gray-700 text-lg leading-7 font-light">
+            <h1 class="font-newsreader text-4xl pt-0 md:pt-5 mb-2 tracking-wide font-medium">Header 1</h1>
+            <p class="mb-4 text-justify">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin tincidunt ut lectus a commodo. Donec cursus, est maximus varius ultricies, sem sem vehicula nibh, eu malesuada turpis lorem ut lectus. Praesent ut tincidunt elit. Mauris eleifend dolor sed elementum scelerisque. Fusce id egestas enim. Nunc commodo lacus dui, id suscipit massa pretium quis. Maecenas gravida lacus eu dapibus lobortis. Donec convallis nisi ac purus suscipit hendrerit. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Aliquam erat volutpat.
+                Curabitur sit amet nisi at dui viverra gravida. Maecenas dapibus ligula vitae fermentum faucibus. Donec suscipit luctus justo in iaculis. Quisque ultricies mi non eros porta, ac iaculis risus accumsan. Donec hendrerit, mi vel varius sodales, ante justo hendrerit ipsum, ut condimentum tellus velit vitae lorem. Phasellus eu vulputate leo. Sed faucibus in augue at iaculis. Nunc faucibus neque vel tincidunt tempor. Nullam sit amet pharetra est. Proin iaculis odio eros, ut mollis tortor ultricies eget. In augue dui, tincidunt at purus fermentum, lacinia ornare urna. In consectetur velit dolor, a facilisis urna interdum eu. Morbi eget risus eget sapien dapibus mattis maximus at leo. Proin justo ligula, efficitur a lacus non, sodales imperdiet erat. Etiam vel urna eu diam tempor lobortis. Phasellus at imperdiet urna.
+            </p>
+            <h2 class="font-newsreader text-3xl pt-0 md:pt-5 mb-2 tracking-wide font-medium">Header 2</h2>
+            <p class="mb-4 md:flex">
+                <img src="https://images.bisnis.com/posts/2021/07/23/1420963/logo-wikipedia.jpg" alt="image" class="max-w-full md:max-w-60 inline-block m-4 w-auto h-auto"/>
+                <span>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin tincidunt ut lectus a commodo. Donec cursus, est maximus varius ultricies, sem sem vehicula nibh, eu malesuada turpis lorem ut lectus. Praesent ut tincidunt elit. Mauris eleifend dolor sed elementum scelerisque. Fusce id egestas enim. Nunc commodo lacus dui, id suscipit massa pretium quis. Maecenas gravida lacus eu dapibus lobortis. Donec convallis nisi ac purus suscipit hendrerit. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Aliquam erat volutpat.</span>
+            </p>
+            <h3 class="font-newsreader text-2xl pt-0 md:pt-5 mb-2 tracking-wide font-medium">Header 3</h3>
+            <p class="mb-4">
+                This is a button. <a href="/" class="bg-blue-500 text-white text-sm rounded-lg py-2 px-7 ml-0 inline-block border border-solid border-blue-500 hover:bg-white hover:text-blue-500 mb-5 md:mb-0">Button</a>
+            </p>
+            <p class="mb-4">
+                This is a link. <a href="/" class="text-blue-500 hover:text-blue-500">Link</a>
+            </p>
+            <h4 class="font-newsreader text-xl pt-0 md:pt-5 mb-2 tracking-wide font-medium">Header 4</h4>
+            <blockquote class="font-inter italic pl-6">
+                <q>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin tincidunt ut lectus a commodo. Donec cursus, est maximus varius ultricies, sem sem vehicula nibh, eu malesuada turpis lorem ut lectus. Praesent ut tincidunt elit. Mauris eleifend dolor sed elementum scelerisque. Fusce id egestas enim.</q>
+            </blockquote>
+            <h5 class="font-newsreader text-lg pt-0 md:pt-5 mb-2 tracking-wide font-medium">Header 5</h5>
+            <ul class="list-disc m-4 p-2">
+                <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</li>
+                <li>Proin tincidunt ut lectus a commodo.</li>
+                <li>Praesent ut tincidunt elit.</li>
+            </ul>
+            <ol class="list-decimal m-4 p-2">
+                <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</li>
+                <li>Proin tincidunt ut lectus a commodo.</li>
+                <li>Praesent ut tincidunt elit.</li>
+            </ol>
+            <table class="w-full border border-solid mt-2 mb-4">
+                <tbody>
+                    <tr>
+                        <th class="p-2 border border-solid">Title</th>
+                        <th class="p-2 border border-solid">Title</th>
+                        <th class="p-2 border border-solid">Title</th>
+                    </tr>
+                    <tr>
+                        <td class="p-2 border border-solid text-sm md:text-base">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</td>
+                        <td class="p-2 border border-solid text-sm md:text-base">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</td>
+                        <td class="p-2 border border-solid text-sm md:text-base">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</td>
+                    </tr>
+                    <tr>
+                        <td class="p-2 border border-solid text-sm md:text-base">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</td>
+                        <td class="p-2 border border-solid text-sm md:text-base">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</td>
+                        <td class="p-2 border border-solid text-sm md:text-base">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</td>
+                    </tr>
+                    <tr>
+                        <td class="p-2 border border-solid text-sm md:text-base">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</td>
+                        <td class="p-2 border border-solid text-sm md:text-base">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</td>
+                        <td class="p-2 border border-solid text-sm md:text-base">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</td>
+                    </tr>
+                </tbody>
+            </table>
+            <table class="w-full mt-2 mb-4">
+                <tbody>
+                    <tr>
+                        <th class="text-left p-2">Title</th>
+                        <th class="text-left p-2">Title</th>
+                        <th class="text-left p-2">Title</th>
+                    </tr>
+                    <tr>
+                        <td class="p-2 text-sm md:text-base">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</td>
+                        <td class="p-2 text-sm md:text-base">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</td>
+                        <td class="p-2 text-sm md:text-base">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</td>
+                    </tr>
+                    <tr>
+                        <td class="p-2 text-sm md:text-base">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</td>
+                        <td class="p-2 text-sm md:text-base">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</td>
+                        <td class="p-2 text-sm md:text-base">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</td>
+                    </tr>
+                    <tr>
+                        <td class="p-2 text-sm md:text-base">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</td>
+                        <td class="p-2 text-sm md:text-base">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</td>
+                        <td class="p-2 text-sm md:text-base">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</td>
+                    </tr>
+                </tbody>
+            </table>
+        </article>
+    </div>
+    <div class="w-full md:w-1/4">
+        @include('partials.sidebar')
+    </div>
+</div>
