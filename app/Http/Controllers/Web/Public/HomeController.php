b8fb22f9234b4f0eadf69117a83341f548601dec
Sonny Sasaka <sonny@jelajahrumah.id>
2024-05-15 21:35:17 -0700

    Publish home page
    Change-Id: I1fef4d0c6720dbf7658cf8192ace95d689df2bff

--- app/Http/Controllers/Web/Public/HomeController.php
+++ app/Http/Controllers/Web/Public/HomeController.php
@@ -4,18 +4,11 @@ namespace App\Http\Controllers\Web\Public;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Contracts\View\View;
-use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
-    public function index(Request $request): View
+    public function index(): View
     {
-        $phase1 = env('PHASE1', false);
-        
-        if ($phase1 == 'true') {
-            return view('home');
-        } else {
-            abort(404, 'Page not found');
-        }
+        return view('home');
     }
 }
