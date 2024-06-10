03005fb29e800aff84fa9281dc0c2e40acd53d07
Sonny Sasaka <sonny@jelajahrumah.id>
2024-06-10 13:30:26 -0700

    Add migration to remove unique index of email
    Change-Id: Ib75776dd76d811c0648f4a984f101fdfaf866f89

---
+++ database/migrations/2024_06_11_032603_remove_unique_from_email.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropIndex(['email']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            // Don't actually re-add the unique index
+            // since rollback may fail with existing data.
+        });
+    }
+};
