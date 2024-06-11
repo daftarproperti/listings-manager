718885a720e7a6344604abf5c7d0bd8f51057d10
Sonny Sasaka <sonny@jelajahrumah.id>
2024-06-11 15:56:16 -0700

    Allow null in ListingUser::$name
    Change-Id: Ic816f7455bfb2ef027b509de9086212b723fcfb9

--- app/Models/ListingUser.php
+++ app/Models/ListingUser.php
@@ -6,8 +6,8 @@ use Spatie\LaravelData\Data;
 
 class ListingUser extends Data
 {
-    public string $name;
-    public ?string $userName;
+    public ?string $name = null;
+    public ?string $userName = null;
     public int $userId;
     public ?string $source = null;
 }
