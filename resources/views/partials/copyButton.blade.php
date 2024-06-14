16fee76bc944c568ec19d819e9a2fda3d44829e5
Sonny Sasaka <sonny@jelajahrumah.id>
2024-06-13 19:49:09 -0700

    Change salin link url
    Change-Id: I3c580d9eae13be805e719997f079f91ed59297a5

--- resources/views/partials/copyButton.blade.php
+++ resources/views/partials/copyButton.blade.php
@@ -1,5 +1,5 @@
 <div>
-    <button type="button" onclick="handleClickCopy('{{ $listing->id }}')" onmouseleave="handleMouseoutCopy('{{ $listing->id }}')" data-tooltip-target="tooltip-{{ $listing->id }}" class="justify-center self-center whitespace-nowrap focus:ring-2 rounded focus:outline-none focus:ring-blue-300 p-2">
+    <button type="button" onclick="handleClickCopy('{{ $listing->listingId }}')" onmouseleave="handleMouseoutCopy('{{ $listing->id }}')" data-tooltip-target="tooltip-{{ $listing->id }}" class="justify-center self-center whitespace-nowrap focus:ring-2 rounded focus:outline-none focus:ring-blue-300 p-2">
         <svg width="20" height="20" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
             <path d="M16 1H4C2.9 1 2 1.9 2 3V17H4V3H16V1ZM15 5H8C6.9 5 6.01 5.9 6.01 7L6 21C6 22.1 6.89 23 7.99 23H19C20.1 23 21 22.1 21 21V11L15 5ZM8 21V7H14V12H19V21H8Z" fill="#64748B" />
         </svg>
