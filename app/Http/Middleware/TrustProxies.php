d033cb564fad59536687b8d0e6d920f1b7e93cd3
Sonny Sasaka <sonny@jelajahrumah.id>
2024-07-18 12:25:33 -0700

    Trust 172.16.0.0/12 as proxy
    This will let Laravel get values set by GAE's proxy like X-Forwarded-For
    
    Change-Id: I791a9fcdc6f1ae13f573a22b9d625b6007867337

--- app/Http/Middleware/TrustProxies.php
+++ app/Http/Middleware/TrustProxies.php
@@ -12,7 +12,7 @@ class TrustProxies extends Middleware
      *
      * @var array<int, string>|string|null
      */
-    protected $proxies = ['127.0.0.1'];
+    protected $proxies = ['127.0.0.1', '172.16.0.0/12'];
 
     /**
      * The headers that should be used to detect proxies.
