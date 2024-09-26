67d8c7ecce09ef2286fa4e60d886cb97b7ce8862
Alvin <alvin@jelajahrumah.id>
2024-09-26 11:14:05 +0700

    feat(daftarproperti): adjust eth sync command to adhere to version defined in env
    Change-Id: I1b529661567ce3f23bb7306684eff5b8622626b1

--- app/Console/Commands/EthSync.php
+++ app/Console/Commands/EthSync.php
@@ -27,38 +27,16 @@ class EthSync extends Command
      */
     protected $description = 'Sync data to Blockchain';
 
-    private function hasListing(Contract $contract, int $id): bool
-    {
-        $exists = false;
-        $contract->call('getListing', $id, function ($err, $ret) use (&$exists) {
-            if ($err !== null) return;
-            $exists = true;
-        });
-        return $exists;
-    }
-
     /**
      * Execute the console command.
      */
     public function handle(): void
     {
-        $abi = type(file_get_contents(storage_path('blockchain/ListingsV0.abi.json')))->asString();
-        $contractAddress = type(env('ETH_LISTINGS_CONTRACT_ADDRESS'))->asString();
-
-        $web3 = new Web3(type(env('ETH_NODE'))->asString());
-        $contract = (new Contract($web3->getProvider(), $abi))->at($contractAddress);
-
         /** @var Collection<int, Listing> $listings */
         $listings = Listing::where('verifyStatus', VerifyStatus::APPROVED)->get();
         foreach ($listings as $listing) {
             $listingId = $listing->listingId;
             $this->line("Processing Listing ID $listingId");
-
-            $exists = $this->hasListing($contract, $listingId);
-            if ($exists) {
-                $this->line("Already in blockchain, skipping");
-                continue;
-            }
 
             Web3Listing::dispatch(
                 $listing,
