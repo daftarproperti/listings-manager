189acba63a632294bc1ee7bd777fc61a9d50e98e
Sonny Sasaka <sonny@jelajahrumah.id>
2024-09-27 14:45:49 -0700

    Add php-cs-fixer to dev requirement
    Change-Id: I3704c045e4c943d40981791791f83511f4540d50

---
+++ .php-cs-fixer.dist.php
@@ -0,0 +1,11 @@
+<?php
+
+$finder = (new PhpCsFixer\Finder())
+    ->in(__DIR__);
+
+return (new PhpCsFixer\Config())
+    ->setRules([
+        '@PSR12' => true,
+        'no_unused_imports' => true,
+    ])
+    ->setFinder($finder);
