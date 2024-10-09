0d2d9a5d947f0a6a145f3885b969d5685fb4dd8a
Sonny Sasaka <sonny@jelajahrumah.id>
2024-10-09 09:52:44 -0700

    Increase throttling limit
    3 per minute has shown to be too restricting, increasing to 10.
    
    Change-Id: I58b9c7aa613c1ab51d7909b98f1302b8fd13518f

--- app/Http/Middleware/ThrottleOTPRequest.php
+++ app/Http/Middleware/ThrottleOTPRequest.php
@@ -10,7 +10,7 @@ class ThrottleOTPRequest
 {
     protected RateLimiter $limiter;
     protected int $decayMinutes = 1;
-    protected int $maxAttempts = 3;
+    protected int $maxAttempts = 10;
 
     public function __construct(RateLimiter $limiter)
     {
