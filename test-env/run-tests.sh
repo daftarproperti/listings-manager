b5acd683ae06c21a39222e46c89809c812edf2bf
Sonny Sasaka <sonny@jelajahrumah.id>
2024-10-16 00:30:19 -0700

    add lint and knip to CI
    Change-Id: Ifae20d82a007200ee95473195a10fee6bcab7e57

--- test-env/run-tests.sh
+++ test-env/run-tests.sh
@@ -6,6 +6,8 @@ composer install
 # To make sure vite manifest is created
 npm install
 npm run build || { echo "Failed npm run build"; exit 1; }
+npm run lint || { echo "Failed lint"; exit 1; }
+npm run knip || { echo "Failed knip"; exit 1; }
 
 ./test-env/wait-mongodb.sh || { echo "MongoDB did not become available, exiting..."; exit 1; }
 
