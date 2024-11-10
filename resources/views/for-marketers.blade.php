543f9a28b87b29f1ecbc9cc7ad27245b49be138a
Sonny Sasaka <sonny@jelajahrumah.id>
2024-11-09 16:54:11 -0800

    standardize rounded to be rounded-lg
    Change-Id: I5c17395283b1317d537b335e5868618753de7a76

--- resources/views/for-marketers.blade.php
+++ resources/views/for-marketers.blade.php
@@ -35,7 +35,7 @@
                         <a
                           target="_blank"
                           href="https://docs.google.com/forms/d/e/1FAIpQLSfsb3FvhXnlx4UDVCG0LPLA0TdUSXm-GorG8sprT98VzST1IA/viewform?usp=sf_link"
-                          class="mb-8 md:mb-16 w-full md:w-auto inline-flex items-center justify-center py-3 px-7 text-base font-semibold text-center text-white rounded-full bg-blue-600 shadow-xs hover:bg-blue-700 transition-all duration-500"
+                          class="mb-8 md:mb-16 w-full md:w-auto inline-flex items-center justify-center py-3 px-7 text-base font-semibold text-center text-white rounded-lg bg-blue-600 shadow-xs hover:bg-blue-700 transition-all duration-500"
                         >
                           Join Waitlist
                           <svg
@@ -57,7 +57,7 @@
                         </a>
                     </div>
                           <div class="flex justify-center items-center  gap-x-5 gap-y-8 lg:gap-y-0 flex-wrap md:flex-wrap lg:flex-nowrap lg:flex-row lg:justify-between lg:gap-x-8">
-                              <div class="group relative w-full bg-gray-100 rounded-2xl p-4 transition-all duration-500 max-md:max-w-md max-md:mx-auto md:w-2/5 md:h-48 xl:p-7 xl:w-1/3 hover:bg-blue-600">
+                              <div class="group relative w-full bg-gray-100 rounded-lg p-4 transition-all duration-500 max-md:max-w-md max-md:mx-auto md:w-2/5 md:h-48 xl:p-7 xl:w-1/3 hover:bg-blue-600">
                                   <h4 class="text-xl font-semibold text-gray-900 mb-3 capitalize transition-all duration-500 group-hover:text-white">High Quality Listings</h4>
                                   <p class="mb-3 text-sm font-normal text-gray-500 transition-all duration-500 leading-5 group-hover:text-white">
                                     Transparent listings with verification, ensuring accuracy, uniqueness, and preventing duplications.
@@ -69,7 +69,7 @@
                                       </svg>
                                   </a>
                               </div>
-                              <div class="group relative w-full bg-gray-100 rounded-2xl p-4 transition-all duration-500 max-md:max-w-md max-md:mx-auto md:w-2/5 md:h-48 xl:p-7 xl:w-1/3 hover:bg-blue-600">
+                              <div class="group relative w-full bg-gray-100 rounded-lg p-4 transition-all duration-500 max-md:max-w-md max-md:mx-auto md:w-2/5 md:h-48 xl:p-7 xl:w-1/3 hover:bg-blue-600">
                                   <h4 class="text-xl font-semibold text-gray-900 mb-3 capitalize transition-all duration-500 group-hover:text-white">Easy Integration</h4>
                                   <p class="mb-3 text-sm font-normal text-gray-500 transition-all duration-500 leading-5 group-hover:text-white">
                                     Use our open-source libraries for data synchronization and referral tracking participation.
@@ -81,7 +81,7 @@
                                       </svg>
                                   </a>
                               </div>
-                              <div class="group relative w-full bg-gray-100 rounded-2xl p-4 transition-all duration-500 max-md:max-w-md max-md:mx-auto md:w-2/5 md:h-48 xl:p-7 xl:w-1/3 hover:bg-blue-600">
+                              <div class="group relative w-full bg-gray-100 rounded-lg p-4 transition-all duration-500 max-md:max-w-md max-md:mx-auto md:w-2/5 md:h-48 xl:p-7 xl:w-1/3 hover:bg-blue-600">
                                   <h4 class="text-xl font-semibold text-gray-900 mb-3 capitalize transition-all duration-500 group-hover:text-white">Fair Referral Tracking</h4>
                                   <p class="mb-3 text-sm font-normal text-gray-500 transition-all duration-500 leading-5 group-hover:text-white">
                                     Fair, open, and auditable referral tracking mechanism to ensure fairness in assigning referrer's commission.
